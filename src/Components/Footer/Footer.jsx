bdc6dee0d2e8a3381eecdd5cb5fdbbfe37545ae8
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-05-13 13:06:19 +0600

    Footer Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="w-full bg-gray-800 py-8 mt-20 text-gray-300 text-center">
             <div className="container mx-auto">
                 <p className="text-base md:text-lg">
-                    &copy; {new Date().getFullYear()} Your StudyBuddy. All Rights Reserved.
+                    &copy; {new Date().getFullYear()} StudyBuddy. All Rights Reserved.
                 </p>
                 <div className="mt-4 md:mt-6">
                     <a href="/about" className="mx-3 md:mx-4 hover:text-gray-400">About</a>
